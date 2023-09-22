e18ade0fddefb9026155ba3e070af24f4f94f50a
dEndivineb <kotikotu13@gmail.com>
2023-09-22 20:22:09 +0200

    Updated adaptivity

---
+++ src/db_config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASw3BWFO9_VNAJAvmIbjTg69O370aOado",
+    authDomain: "todo-app-6d8fc.firebaseapp.com",
+    databaseURL: "https://todo-app-6d8fc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-app-6d8fc",
+    storageBucket: "todo-app-6d8fc.appspot.com",
+    messagingSenderId: "579218758699",
+    appId: "1:579218758699:web:04cf3d0757e849572e9e6e"
+}
+
+export default firebaseConfig
